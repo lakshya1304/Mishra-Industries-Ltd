66a2e76e69f06a18eeab2e971f120a19fb3b6a45
lakshya1304 <lakshya.13.goyal@gmail.com>
2026-02-24 18:38:07 +0530

    bug

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import db from "./config/db.js";
 import app from "./config/server.js";
-<<<<<<< HEAD
+
 import "dotenv/config";
+
 // Immediately Invoked Async Function Expression
 (async () => {
   try {
@@ -12,13 +13,8 @@ import "dotenv/config";
     process.exit(1);
   }
 
-=======
-import "dotenv/config"
-// Immediately Invoked Async Function Expression
-(async () => {
-  await db();
->>>>>>> d6af449afae658f3056276f162bee9b4028035ad
   const PORT = process.env.PORT || 5000;
+
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
